d1bffcfbe11d3d36a3b208a691b0ee1657fe01eb
zulfadesign <zulfaindana299@gmail.com>
2026-05-28 11:15:54 +0700

    Add files via upload

---
+++ build/classes/Pertemuan2/Pertemuan3/perulanganWHILE.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan3;
+
+/**
+ *
+ * @author achma
+ */
+public class perulanganWHILE {
+    public static void main(String[] args) {
+        int i = 1;
+        
+        while (i <= 5){
+            System.out.println(i+"");
+        }
+    }
+}
